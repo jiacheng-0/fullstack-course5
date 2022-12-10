795a2dc94e225fd51e0b6ae5457db4e5702aea5f
Teo, Jia Cheng <jiacteo@visa.com>
2022-12-10 21:21:26 +0800

    format

--- examples/Lecture20/app.js
+++ examples/Lecture20/app.js
@@ -1,57 +1,59 @@
 (function () {
-'use strict';
+    'use strict';
 
-angular.module('ShoppingListApp', [])
-.controller('ShoppingListAddController', ShoppingListAddController)
-.controller('ShoppingListShowController', ShoppingListShowController)
-.service('ShoppingListService', ShoppingListService);
+    angular.module('ShoppingListApp', [])
+        .controller('ShoppingListAddController', ShoppingListAddController)
+        .controller('ShoppingListShowController', ShoppingListShowController)
+        .service('ShoppingListService', ShoppingListService);
 
-ShoppingListAddController.$inject = ['ShoppingListService'];
-function ShoppingListAddController(ShoppingListService) {
-  var itemAdder = this;
+    ShoppingListAddController.$inject = ['ShoppingListService'];
 
-  itemAdder.itemName = "";
-  itemAdder.itemQuantity = "";
+    function ShoppingListAddController(ShoppingListService) {
+        var itemAdder = this;
 
-  itemAdder.addItem = function () {
-    ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
-  }
-}
+        itemAdder.itemName = "";
+        itemAdder.itemQuantity = "";
 
-
-ShoppingListShowController.$inject = ['ShoppingListService'];
-function ShoppingListShowController(ShoppingListService) {
-  var showList = this;
-
-  showList.items = ShoppingListService.getItems();
-
-  showList.removeItem = function (itemIndex) {
-    ShoppingListService.removeItem(itemIndex);
-  };
-}
+        itemAdder.addItem = function () {
+            ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+        }
+    }
 
 
-function ShoppingListService() {
-  var service = this;
+    ShoppingListShowController.$inject = ['ShoppingListService'];
 
-  // List of shopping items
-  var items = [];
+    function ShoppingListShowController(ShoppingListService) {
+        var showList = this;
 
-  service.addItem = function (itemName, quantity) {
-    var item = {
-      name: itemName,
-      quantity: quantity
-    };
-    items.push(item);
-  };
+        showList.items = ShoppingListService.getItems();
 
-  service.removeItem = function (itemIndex) {
-    items.splice(itemIndex, 1);
-  };
+        showList.removeItem = function (itemIndex) {
+            ShoppingListService.removeItem(itemIndex);
+        };
+    }
 
-  service.getItems = function () {
-    return items;
-  };
-}
+
+    function ShoppingListService() {
+        var service = this;
+
+        // List of shopping items
+        var items = [];
+
+        service.addItem = function (itemName, quantity) {
+            var item = {
+                name: itemName,
+                quantity: quantity
+            };
+            items.push(item);
+        };
+
+        service.removeItem = function (itemIndex) {
+            items.splice(itemIndex, 1);
+        };
+
+        service.getItems = function () {
+            return items;
+        };
+    }
 
 })();
